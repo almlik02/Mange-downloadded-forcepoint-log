038161d2bb4cada06fe0c874d2f0a5e83366dd5a
almlik02 <162103196+almlik02@users.noreply.github.com>
2024-03-04 12:27:11 +0400

    Add files via upload
    Description:
    
    1. Define Variables:
    
    sourceFolder: Specifies the folder containing the .gz files to be decompressed.
    destinationFolder: Specifies the folder where the decompressed files will be stored.
    gzipExePath: Specifies the path to the 7-Zip executable.
    logFile: Specifies the path to the log file where processed file names will be stored.
    
    2. Ensure Destination Folder Exists:
    
    Checks if the destination folder exists and creates it if it doesn't.
    
    3. Create Log File:
    
    Checks if the log file exists and creates it if it doesn't.
    
    4. Decompress .gz Files:
    
    Loops through each .gz file in the source folder.
    Checks if the file has already been processed by looking at the log file.
    If the file hasn't been processed:
    Decompresses the .gz file to the destination folder using 7-Zip.
    If the decompression is successful, the original .gz file is deleted, and the file name is added to the log.
    If the decompression fails, an error message is logged.
    
    5. Delete Old Files:
    
    Uses PowerShell to delete decompressed files older than 1 hour in the destination folder.
    This script can be used to automate the decompression of .gz files and manage the decompressed files efficiently.

---
+++ script.sh
@@ -0,0 +1,35 @@
+@echo off
+SET sourceFolder="G:\zipfolder"
+SET destinationFolder="G:\unzipfolder"
+SET gzipExePath="C:\Program Files\7-Zip\7z.exe"
+SET logFile="%destinationFolder%\processed_files.txt"
+ 
+:: Ensure the destination folder exists
+if not exist "%destinationFolder%" mkdir "%destinationFolder%"
+ 
+:: Create log file if it doesn't exist
+if not exist "%logFile%" type nul > "%logFile%"
+ 
+:: Decompress all .gz files in the source folder to the destination folder
+for %%i in ("%sourceFolder%\*.gz") do (
+    :: Check if file is in the log file (already processed)
+    findstr /i /c:"%%~ni" "%logFile%" >nul
+    if errorlevel 1 (
+        pushd "%destinationFolder%"
+        echo Extracting %%i...
+        %gzipExePath% e "%%i" -so > "%%~ni.csv" 2>"extract_error.log"
+        if not errorlevel 1 (
+            echo Successfully extracted: %%~ni
+            del "%%i" /f /q
+            echo %%~ni >> "%logFile%"
+        ) else (
+            echo Extraction failed for %%i. Check extract_error.log for details.
+        )
+        popd
+    ) else (
+        echo Skipping duplicate file: %%~ni
+    )
+)
+ 
+:: Delete decompressed files older than 1 hour using PowerShell
+forfiles /p "%destinationFolder%" /m * /c "cmd /c if @isdir==FALSE (powershell -command \"if ((Get-Item @path).LastWriteTime -lt (Get-Date).AddHours(-1)) {Remove-Item @path -Force}\")"
